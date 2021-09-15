9305cf85afabf4926097410ba1752a2dd9a62ae1
Cristi Gutierrez <cristigtzname@gmail.com>
2021-09-14 20:13:21 -0400

    JS

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 let computerGuess;
+let userGuesses = [];
+let attempts = 0;
+let maxGuesses;
+
+function gameEnded() {
+    document.getElementById('newGameButton').style.display = "inline";
+    document.getElementById('inputBox').setAttribute('readonly', 'readonly')
+}
 
 function init() {
     computerGuess = Math.floor(Math.random() * 100 + 1);
+    console.log(computerGuess);
     document.getElementById('newGameButton').style.display = "none";
     document.getElementById('gameArea').style.display = "none";
 }
@@ -12,9 +21,48 @@ function startGameView() {
 }
 
 function easyMode() {
+    maxGuesses = 10;
     startGameView();
 }
 
 function hardMode() {
+    maxGuesses = 5;
     startGameView();
+}
+
+function compareGuess() {
+    const userGuess = Number(document.getElementById('inputBox').value);
+    userGuesses.push(" " + userGuess);
+    document.getElementById('guesses').innerHTML = userGuesses;
+    attempts++
+    document.getElementById("attempts").innerHTML = attempts;
+    if (attempts < maxGuesses) {
+        if (userGuess > computerGuess) {
+            document.getElementById("textOutput").innerHTML = "Your guess is too high";
+            document.getElementById('inputBox').value = "";
+        } else if (userGuess < computerGuess) {
+            document.getElementById("textOutput").innerHTML = "Your guess is too low";
+            document.getElementById('inputBox').value = "";
+        } else {
+            document.getElementById("textOutput").innerHTML = "Correct! You got it in " + attempts + " attempts.";
+            gameEnded();
+        }
+    } else {
+        if (userGuess > computerGuess) {
+            document.getElementById("textOutput").innerHTML = "YOU LOSE! <br> The number was " + computerGuess;
+            gameEnded();
+
+        } else if (userGuess < computerGuess) {
+            document.getElementById("textOutput").innerHTML = "YOU LOSE! <br> The number was " + computerGuess;
+            gameEnded();
+
+        } else {
+            document.getElementById("textOutput").innerHTML = "Correct! You got it in " + attempts + " attempts.";
+            gameEnded();
+        }
+    }
+}
+
+function reloadPage() {
+    window.location.reload();
 }
